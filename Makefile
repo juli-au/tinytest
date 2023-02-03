e50ee348da4ef8cf2159b7bcea4804bc751494f9
Julia Ogris <julia.ogris@gmail.com>
2023-02-03 13:35:47 +1100

    Prototype concurrency with sleep, no channels, no sys/js

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ O = out
 # target for static HTML content, GitHub pages only supports <root> and docs/
 STATIC = docs
 
-all: build lint tiny ## Build and lint
+all: lint tiny ## Build and lint
 	@if [ -e .git/rebase-merge ]; then git --no-pager log -1 --pretty='%h %s'; fi
 	@echo '$(COLOUR_GREEN)Success$(COLOUR_NORMAL)'
 
@@ -24,15 +24,8 @@ clean:: ## Remove generated files
 
 # --- Build and serve ----------------------------------------------------------
 
-build: | $(O) ## Build binaries
-	go build -o $(O) .
-
-# Use `go version` to ensure the right go version is installed when using tinygo.
-go-version:
-	go version
-
 # Optimise tinygo output for size, see https://www.fermyon.com/blog/optimizing-tinygo-wasm
-tiny: go-version | $(O) ## Build for tinygo / wasm
+tiny: | $(O) ## Build for tinygo / wasm
 	tinygo version
 	tinygo build -o $(STATIC)/tinytest.wasm -target wasm -no-debug .
 	cp -f $$(tinygo env TINYGOROOT)/targets/wasm_exec.js $(STATIC)
@@ -48,7 +41,7 @@ clean::
 	-rm -f $(STATIC)/tinytest.wasm
 	-rm -f $(STATIC)/wasm_exec.js
 
-.PHONY: build decompile go-version install tidy tiny
+.PHONY: decompile serve tiny
 
 # --- Lint and tidy ------------------------------------------------------------
 
@@ -58,7 +51,7 @@ tidy: ## Tidy go modules with "go mod tidy"
 lint: ## Lint go source code
 	golangci-lint run
 
-.PHONY: lint
+.PHONY: lint tidy
 
 # --- Utilities ----------------------------------------------------------------
 COLOUR_NORMAL = $(shell tput sgr0 2>/dev/null)
--- main.go
+++ main.go
@@ -1,23 +1,105 @@
 //nolint:unused
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-//export hello
-func hello() {
-	println("hello:", time.Now().Format("15:04:05.0"))
+var cancelled = false
+
+var mousemoveEventX = []int{}
+
+//TODO: var events = []any //with type switch for even type
+
+func main() {
+	start := time.Now()
+	fmt.Println(start.Format("05.0"), "main: started")
+	busy()
+
+	fmt.Println("main: registerMousemove")
+	registerMousemove()
+
+	// registerMouseUp()
+	// registerMouseDown()
+	// registerKeyPress()
+	// registerAnimate()...
+
+	fmt.Println("main: handleEvents")
+	handleEvents()
+
+	// todo: if cancelled: unregister events.
+	fmt.Println(time.Now().Format("05.0"), "main finished in", time.Since(start).String())
+	select {}
 }
 
-//export sleep
-func sleep() {
-	for i := 1; i < 10; i++ {
-		println("sleep", i, ":", time.Now().Format("15:04:05.0"))
-		time.Sleep(time.Second)
+func busy() {
+	fmt.Println("busy: started")
+	cancelled = false
+	sec := time.Now().Format("05.0")
+	start := time.Now()
+	for i := 1; i < 30000; i++ {
+		if i%100 == 0 && time.Since(start) > time.Millisecond*25 {
+			time.Sleep(time.Millisecond)
+			start = time.Now()
+		}
+		if cancelled {
+			fmt.Println("busy: cancelled")
+			return
+		}
+		s := fmt.Sprintf("%s %s %d\n", sec, "busy", i)
+		fmt.Print(s)
+	}
+	fmt.Println("busy: done")
+}
+
+func handleEvents() {
+	cancelled = false
+	for {
+		time.Sleep(time.Millisecond)
+		if len(mousemoveEventX) > 0 {
+			lastIdx := len(mousemoveEventX) - 1
+			x := mousemoveEventX[lastIdx]
+			mousemoveEventX = mousemoveEventX[:lastIdx]
+			fmt.Println("handleEvents: mousemove x", x)
+		}
+		if cancelled {
+			fmt.Println("eventHandler cancelled")
+			return
+		}
 	}
 }
 
-func main() {
-	println("main 1 :", time.Now().Format("15:04:05.0"))
-	sleep()
-	println("main 2 :", time.Now().Format("15:04:05.0"))
+//export cancel
+func cancel() {
+	fmt.Println("cancel click")
+	cancelled = true
 }
+
+//export registerMousemove
+func registerMousemove()
+
+//export mousemove
+func mousemove(x, y int) {
+	mousemoveEventX = append(mousemoveEventX, x)
+}
+
+/*
+// TODO: implement in JS, maybe with logic to only return string after \n ...
+//export jsTextInput
+func jsTextInput() string
+
+//export jsResetInput
+func jsResetInput(string)
+
+func readInput() string {
+	for !cancelled {
+		if s := jsTextInput(); s != "" {
+			jsResetInput(s)
+			return s
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+	return "" // cancelled
+}
+*/
